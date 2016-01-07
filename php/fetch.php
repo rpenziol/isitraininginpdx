82a084d4e4036bd63b57b80365b9996372c2278f
Robbie Penziol <fizzycakes@hotmail.com>
2016-01-06 18:11:08 -0800

    Changed fetch command to loop every 3min indefinitely, to avoid dependence on crontab (which stopped working on my system)

--- php/fetch.php
+++ php/fetch.php
@@ -2,9 +2,16 @@
 <?php
   $api_key = file_get_contents("api_url");
   $api_key = str_replace("\n", '', $api_key);
-  $json_string = file_get_contents($api_key);
-  $parsed_json = json_decode($json_string);
-  $location = $parsed_json->{'location'}->{'city'};
-  $condition = $parsed_json->{'current_observation'}->{'weather'};
-  echo "${condition}";
+  
+  while(true) {
+  	$json_string = file_get_contents($api_key); //Fetch condition information
+  	$parsed_json = json_decode($json_string);
+  	$location = $parsed_json->{'location'}->{'city'};
+  	$condition = $parsed_json->{'current_observation'}->{'weather'};
+  
+  	$fp = fopen("current_condition.txt" , "w");
+  	fputs($fp , $condition);
+  	fclose($fp);
+        sleep(180);
+  }
 ?>
