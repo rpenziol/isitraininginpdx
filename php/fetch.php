b595c99bf7e19adca325bf229890017a4ea85fe2
Robbie Penziol <rpenziol@pdx.edu>
2018-01-11 18:59:33 -0800

    Cleaned up fetch script

--- php/fetch.php
+++ php/fetch.php
@@ -1,14 +1,10 @@
-#!/usr/local/bin/php
+#!/usr/bin/php
 <?php
   $api_url = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . '/api_url');
   $api_url = str_replace("\n", '', $api_url);
   
-  while(true) {
-  	$condition = file_get_contents($api_url); //Fetch condition information
-  
-  	$fp = fopen(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'condition.json' , "w");
-  	fputs($fp , $condition);
-  	fclose($fp);
-    sleep(180);
-  }
+  $condition = file_get_contents($api_url); //Fetch condition information
+  $fp = fopen(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'condition.json' , "w");
+  fputs($fp , $condition);
+  fclose($fp);
 ?>
