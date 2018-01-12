f8ae5bd126679d30e1fb1f741673757155a5496e
Robbie Penziol <rpenziol@users.noreply.github.com>
2018-01-11 22:53:40 -0800

    Fixed bug in path of fetch script. Re-designed to run as an infinite pulling loop.

--- php/fetch.php
+++ php/fetch.php
@@ -1,10 +1,13 @@
 #!/usr/bin/php
 <?php
-  $api_url = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . '/api_url');
-  $api_url = str_replace("\n", '', $api_url);
+  while(True) {
+    $api_url = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'api_url');
+    $api_url = str_replace("\n", '', $api_url);
   
-  $condition = file_get_contents($api_url); //Fetch condition information
-  $fp = fopen(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'condition.json' , "w");
-  fputs($fp , $condition);
-  fclose($fp);
+    $condition = file_get_contents($api_url); //Fetch condition information
+    $fp = fopen(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'condition.json' , "w");
+    fputs($fp , $condition);
+    fclose($fp);
+    sleep(180);
+  }
 ?>
